6cec896df6026ab3a719547392bbd2497374b61a
Kyle Dotterrer <kdotterrer@gmail.com>
2020-03-10 21:28:24 -0400

    playing with std::bind

---
+++ Functional/Bind.cpp
@@ -0,0 +1,74 @@
+// Bind.cpp
+// Basic usage of std::bind.
+//
+// Build
+//  cl /EHsc /nologo /std:c++17 /W4 /I c:\Dev\Catch2 Bind.cpp
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <functional>
+
+long multiply(long a, long b)
+{
+    return a*b;
+}
+
+TEST_CASE("std::bind supports binding free functions")
+{
+    SECTION("with fully applied function")
+    {
+        auto fn = std::bind(multiply, 2, 10);
+        
+        auto result = fn();
+        REQUIRE(result == 20);
+    }
+
+    SECTION("with partially applied function")
+    {
+        auto times_ten = std::bind(multiply, std::placeholders::_1, 10);
+        auto times_two = std::bind(multiply, 2, std::placeholders::_1);
+
+        REQUIRE(times_ten(2) == 20);
+        REQUIRE(times_two(10) == 20);
+    }
+}
+
+struct Computer
+{
+    long m_a;
+    long m_b;
+
+    Computer(long a, long b) : m_a{a}, m_b{b} {}
+
+    long multiply()
+    {
+        return m_a*m_b;
+    }
+
+    long sum(long c)
+    {
+        return m_a+m_b+c;
+    }
+};
+
+TEST_CASE("std::bind suports binding member functions")
+{
+    SECTION("with fully applied function")
+    {
+        auto c = Computer{2, 10};
+
+        auto fn = std::bind(&Computer::multiply, std::placeholders::_1);
+        REQUIRE(fn(c) == 20);
+    }
+
+    SECTION("with partially applied function")
+    {
+        auto c = Computer{2, 10};
+
+        auto fn = std::bind(&Computer::sum, c, std::placeholders::_1);
+        REQUIRE(fn(8) == 20);
+    }
+}
+
+
---
+++ Functional/BindVsLambda.cpp
@@ -0,0 +1,95 @@
+// BindVsLambda.cpp
+// Quick comparison of using std::bind versus lambdas.
+//
+// Build
+//  cl /EHsc /nologo /std:c++17 /W4 /I c:\Dev\Catch2 BindVsLambda.cpp
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+long multiply(long a, long b)
+{
+    return a*b;
+}
+
+TEST_CASE("std::bind and lambdas can both bind fully-applied free functions")
+{
+    SECTION("std::bind")
+    {
+        auto fn = std::bind(multiply, 2, 10);
+        REQUIRE(fn() == 20);
+    }
+
+    SECTION("lambda")
+    {
+        auto fn = [](){ return multiply(2, 10); };
+        REQUIRE(fn() == 20);
+    }
+}
+
+TEST_CASE("std::bind and lambdas can both bind partially-applied functions")
+{
+    SECTION("std::bind")
+    {
+        auto fn = std::bind(multiply, 2, std::placeholders::_1);
+        REQUIRE(fn(10) == 20);
+    }
+
+    SECTION("lambda")
+    {
+        auto fn = [](long b){ return multiply(2, b); };
+        REQUIRE(fn(10) == 20);
+    }
+}
+
+template <typename Callable>
+void do_work_and_callback(Callable fn)
+{
+    // do some heavy work here
+    fn();
+}
+
+struct AsyncWorkerWithBind
+{   
+    bool m_completed{false};
+
+    void start_work()
+    {
+        auto cb = std::bind(&AsyncWorkerWithBind::on_done, this);
+        do_work_and_callback(cb);
+    }
+
+    void on_done() { m_completed = true; }
+    bool done() const noexcept { return m_completed; }
+};
+
+struct AsyncWorkerWithLambda
+{
+    bool m_completed{false};
+
+    void start_work()
+    {
+        auto cb = [this](){ on_done(); };
+        do_work_and_callback(cb);
+    }
+
+    void on_done() { m_completed = true; }
+    bool done() const noexcept { return m_completed; }
+};
+
+TEST_CASE("std::bind and lambdas can both be used to bind member functions")
+{
+    SECTION("std::bind")
+    {
+        auto worker = AsyncWorkerWithBind{};
+        worker.start_work();
+        REQUIRE(worker.done());
+    }
+
+    SECTION("lambda")
+    {
+        auto worker = AsyncWorkerWithLambda{};
+        worker.start_work();
+        REQUIRE(worker.done());
+    }
+}
