99f4811d294fe8572922c2380e8266e6c05ddbeb
kyledotterrer <kdotterrer@gmail.com>
2020-03-17 18:56:25 -0400

    unique_ptr

--- SmartPointers/unique_ptr.cpp
+++ SmartPointers/unique_ptr.cpp
@@ -7,7 +7,135 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
-TEST_CASE("")
+#include <memory>
+
+class X
 {
-    REQUIRE(true);
+    int m_id;
+
+public:
+    X(int id) : m_id{ id }
+    {
+        ++count;
+    }
+
+    ~X() 
+    { 
+        --count; 
+    }
+
+    int get_id() const noexcept
+    {
+        return m_id;
+    }
+
+    static int count;
+};
+
+int X::count = 0;
+
+TEST_CASE("std::unique_ptr supports exclusive, transferrable ownership semantics")
+{
+    auto p1 = std::unique_ptr<X>{new X{1337}};
+    REQUIRE(X::count == 1);
+
+    SECTION("with move construction")
+    {
+        auto p2 = std::unique_ptr<X>{std::move(p1)};
+        REQUIRE(X::count == 1);
+        REQUIRE(p2->get_id() == 1337);
+    }
+
+    SECTION("with move assignment")
+    {
+        auto p2 = std::unique_ptr<X>{new X{1054}};
+        
+        REQUIRE(X::count == 2);
+
+        p2 = std::move(p1);
+
+        REQUIRE(X::count == 1);
+        REQUIRE(p2->get_id() == 1337);
+    }
+}
+
+TEST_CASE("std::make_unique() provides convenient construction of std::unique_ptr")
+{
+    REQUIRE(X::count == 0);
+
+    {
+        auto p = std::make_unique<X>(1337);
+
+        REQUIRE(X::count == 1);
+        REQUIRE(p->get_id() == 1337);
+    }
+
+    REQUIRE(X::count == 0);
+}
+
+TEST_CASE("std::unique_ptr supports other member functions")
+{
+    SECTION("std::unique_ptr::swap() and std::swap()")
+    {
+        auto p1 = std::make_unique<X>(1337);
+        auto p2 = std::make_unique<X>(1054);
+
+        REQUIRE(p1->get_id() == 1337);
+        REQUIRE(p2->get_id() == 1054);
+
+        p1.swap(p2);
+
+        REQUIRE(p1->get_id() == 1054);
+        REQUIRE(p2->get_id() == 1337);
+
+        std::swap(p1, p2);
+
+        REQUIRE(p1->get_id() == 1337);
+        REQUIRE(p2->get_id() == 1054);
+    }
+
+    SECTION("std::unique_ptr::reset()")
+    {
+        auto p1 = std::make_unique<X>(1337);
+
+        REQUIRE(static_cast<bool>(p1));
+
+        p1.reset();
+
+        REQUIRE_FALSE(static_cast<bool>(p1));
+
+        auto p2 = std::make_unique<X>(1054);
+
+        p1.reset(p2.release());
+
+        REQUIRE_FALSE(static_cast<bool>(p2));
+        REQUIRE(static_cast<bool>(p1));
+        REQUIRE(p1->get_id() == 1054);
+    }
+
+    SECTION("std::unique_ptr::release()")
+    {
+        auto p = std::make_unique<X>(1337);
+
+        auto raw = p.release();
+
+        REQUIRE_FALSE(static_cast<bool>(p));
+        REQUIRE(raw->get_id() == 1337);
+
+        delete raw;
+    }
+
+    SECTION("std::unique_ptr::get()")
+    {
+        {
+            auto p = std::make_unique<X>(1337);
+            REQUIRE(X::count == 1);
+
+            auto raw = p.get();
+            REQUIRE(raw->get_id() == 1337);
+        }
+
+        // ownership maintained; destructor executed
+        REQUIRE(X::count == 0);
+    }
 }
