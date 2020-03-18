26f6ae4f6366f1959a7ae311e19f30c309ca3682
Kyle Dotterrer <kdotterrer@gmail.com>
2020-03-18 18:18:49 -0400

    ranges

---
+++ Ranges/composability.cpp
@@ -0,0 +1,33 @@
+// composability.cpp
+// Demo of basic range utility: composability.
+//
+// Build
+//  cl /EHsc /nologo /W4 /std:c++latest /permissive- /experimental:preprocessor /I C:\Dev\Catch2 /I C:\Dev\Rangev3\Include composability.cpp
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+#include <range/v3/all.hpp>
+
+TEST_CASE("ranges enable simple and elegant composition of operations on containers")
+{
+    auto v = std::vector<int>(10);
+    std::iota(v.begin(), v.end(), 0);
+
+    // filter out odd values,
+    // filter out values less than 4,
+    // and square remaining values
+    auto r = v | ranges::views::remove_if([](int i){ return i % 2 != 0; })
+               | ranges::views::remove_if([](int i){ return i < 4; })
+               | ranges::views::transform([](int i){ return i*i; })
+               | ranges::to<std::vector>();  
+
+    auto expected = std::vector<int>{16, 36, 64};
+    
+    REQUIRE(r.size() == expected.size());
+    REQUIRE(std::equal(r.begin(), r.end(), expected.begin()));
+}
