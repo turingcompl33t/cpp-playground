8d2ab547b963e497a548b75b4d076edbd298e8e7
Kyle Dotterrer <kdotterrer@gmail.com>
2020-03-23 21:42:12 -0400

    charconv

---
+++ Utilities/Charconv/from_chars.cpp
@@ -0,0 +1,23 @@
+// from_chars.cpp
+//
+// Demo of <charconv>'s from_chars()
+//
+// Build
+//  cl /EHsc /nologo /std:c++17 /W4 /I C:\Dev\Catch2 from_chars.cpp
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <array>
+#include <charconv>
+
+TEST_CASE("std::from_chars() basic usage")
+{
+    auto str = std::array<char, 16>{"42"};
+    auto res = int{};
+
+    auto [p, ec] = std::from_chars(str.data(), str.data() + str.size(), res);
+
+    REQUIRE(ec == std::errc{});
+    REQUIRE(res == 42);
+}
---
+++ Utilities/Charconv/to_chars.cpp
@@ -0,0 +1,25 @@
+// to_chars.cpp
+//
+// Demo of <charconv>'s to_chars()
+//
+// Build
+//  cl /EHsc /nologo /std:c++17 /W4 /I C:\Dev\Catch2 to_chars.cpp
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <array>
+#include <charconv>
+#include <string_view>
+
+TEST_CASE("std::to_chars() basic usage")
+{
+    // space for conversion output
+    auto str = std::array<char, 16>{};
+
+    // return value is std::to_chars_result
+    auto [p, ec] = std::to_chars(str.data(), str.data() + str.size(), 42);
+
+    REQUIRE(ec == std::errc{});  // no error
+    REQUIRE(std::string_view(str.data(), p - str.data()) == "42");
+}
