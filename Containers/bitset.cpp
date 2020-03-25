0554dbd7729d2d97150f872b9e4397cf1af285f7
Kyle Dotterrer <kdotterrer@gmail.com>
2020-03-25 10:41:24 -0400

    bitset

---
+++ Containers/bitset.cpp
@@ -0,0 +1,65 @@
+// bistset.cpp
+// Demo of basic properties and usage of std::bitset.
+//
+// Build
+//  cl /EHsc /nologo /std:c++17 /W4 /I c:\Dev\Catch2 bitset.cpp
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <bitset>
+#include <limits>
+
+enum Color
+{
+    red,
+    yellow, 
+    green,
+    blue,
+    n_colors
+};
+
+TEST_CASE("std::bitset can be used to maintain a set of flags")
+{
+    auto c = std::bitset<Color::n_colors>{};
+
+    c.set(Color::red);
+    c.set(Color::blue);
+
+    REQUIRE(c.count() == 2);
+    
+    REQUIRE(c.test(Color::red));
+    REQUIRE(c.test(Color::blue));
+    REQUIRE_FALSE(c.test(Color::green));
+}
+
+TEST_CASE("std::bitset can be used to convert integral values to their binary representation")
+{
+    SECTION("unsigned short")
+    {
+        auto c = std::bitset<std::numeric_limits<unsigned short>::digits>(267);
+        REQUIRE(c.to_string() == "0000000100001011");
+    }
+
+    SECTION("unsigned long")
+    {
+        auto c = std::bitset<std::numeric_limits<unsigned long>::digits>(267);
+        REQUIRE(c.to_string() == "00000000000000000000000100001011");
+    }
+}
+
+TEST_CASE("std:bitset can be used to convert binary representations to integral values")
+{
+    SECTION("unsigned short")
+    {
+        auto c = std::bitset<std::numeric_limits<unsigned short>::digits>("0000000100001011");
+        REQUIRE(c.to_ulong() == 267);
+    }
+
+    SECTION("unsigned long")
+    {
+        auto c = std::bitset<std::numeric_limits<unsigned long>::digits>(
+            "00000000000000000000000100001011");
+        REQUIRE(c.to_ulong() == 267);
+    }
+}
