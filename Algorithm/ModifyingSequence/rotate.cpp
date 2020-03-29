d8f7ff1fdf6d812b4c199433fbb3f214a6438607
kyledotterrer <kdotterrer@gmail.com>
2020-03-28 20:25:12 -0400

    rotate

--- Algorithm/ModifyingSequence/rotate.cpp
+++ Algorithm/ModifyingSequence/rotate.cpp
@@ -13,16 +13,49 @@
 #include <iostream>
 #include <algorithm>
 
-TEST_CASE("std::rotate()")
+TEST_CASE("std::rotate() can rotate an entire range")
 {
     auto c = std::vector<int>(10);
-    std::iota(c.begin(), c.end(), 0);  // 0 - 9
+    std::iota(c.begin(), c.end(), 1);  // 1 - 10
 
-    REQUIRE(c.front() == 0);
-    REQUIRE(c.back() == 9);
+    REQUIRE(c.front() == 1);
+    REQUIRE(c.back() == 10);
 
-    std::rotate(c.begin(), c.begin() + 4, c.end());
+    std::rotate(c.begin(), c.begin() + 3, c.end());
 
     REQUIRE(c.front() == 4);
     REQUIRE(c.back() == 3);
+}
+
+TEST_CASE("std::rotate() can rotate a subrange within a larger range")
+{
+    auto c = std::vector<int>(10);
+    std::iota(std::begin(c), std::end(c), 1); // 1 - 10
+
+    REQUIRE(*(std::begin(c)+1) == 2);
+    REQUIRE(*(std::begin(c)+4) == 5);
+
+    std::rotate(std::begin(c) + 1, std::begin(c) + 4, std::begin(c) + 5);
+
+    REQUIRE(*(std::begin(c)+1) == 5);
+    REQUIRE(*(std::begin(c)+4) == 4);
+}
+
+TEST_CASE("std::rotate() can be combined with std::stable_parition()\
+    to perform some relatively complex modifying sequence operations")
+{
+    auto c = std::vector<int>(10);
+    std::iota(std::begin(c), std::end(c), 1); // 1 - 10
+
+    auto even = [](int i){ return i % 2 == 0; };
+
+    // move all even numbers to the front of the container
+    auto p = std::stable_partition(std::begin(c), std::end(c), even);
+
+    // rotate the entire subrange
+    std::rotate(std::begin(c), p, p + 1);
+
+    auto it = std::begin(c);
+    REQUIRE(*it++ == 1);
+    REQUIRE(*it++ == 2);
 }
