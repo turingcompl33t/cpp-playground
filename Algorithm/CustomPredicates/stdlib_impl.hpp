8790637f894877ec194947d6fc46ea1a1dd236ff
kyledotterrer <kdotterrer@gmail.com>
2020-04-17 22:23:27 -0400

    updates to algorithm

--- Algorithm/CustomPredicates/stdlib_impl.hpp
+++ Algorithm/CustomPredicates/stdlib_impl.hpp
@@ -18,8 +18,8 @@ struct contains
     template <typename Iter1, typename Iter2>
     bool operator()(Iter1 begin1, Iter1 end1, Iter2 begin2, Iter2 end2) const
     {
-        return std::distance(begin2, end2) == 0 || 
-            std::search(begin1, end1, begin2, end2, Comparer()) != end1;
+        return std::distance(begin2, end2) == 0
+            || std::search(begin1, end1, begin2, end2, Comparer()) != end1;
     }
 };
 
@@ -29,20 +29,8 @@ struct starts_with
     template <typename Iter1, typename Iter2>
     bool operator()(Iter1 begin1, Iter1 end1, Iter2 begin2, Iter2 end2) const
     {
-        const auto dist1 = std::distance(begin1, end1);
-        const auto dist2 = std::distance(begin2, end2);
-
-        // always starts with empty range
-        if (dist2 == 0)
-            return true;
-
-        if (dist2 > dist1)
-            return false;
-
-        auto prefix_end = begin1;
-        std::advance(prefix_end, dist2);
-
-        return std::equal(begin1, prefix_end, begin2, end2, Comparer());
+        const auto first_nonequal = std::mismatch(begin1, end1, begin2, end2, Comparer());
+        return first_nonequal.second == end2;
     }
 };
 
@@ -55,16 +43,10 @@ struct ends_with
         const auto dist1 = std::distance(begin1, end1);
         const auto dist2 = std::distance(begin2, end2);
 
-        // always ends with empty range
-        if (dist2 == 0)
-            return true;
-
         if (dist2 > dist1)
             return false;
 
-        auto suffix_begin = begin1;
-        std::advance(suffix_begin, dist1 - dist2);
-
+        const auto suffix_begin = std::next(begin1, dist1 - dist2);
         return std::equal(suffix_begin, end1, begin2, end2, Comparer());
     }
 };
