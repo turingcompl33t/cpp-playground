8864fd96a2f150764603a5e1763e07d4535b973a
Kyle Dotterrer <kdotterrer@gmail.com>
2020-05-23 11:50:52 -0400

    monotonic buffer resource

---
+++ Allocators/monotonic_buffer.cpp
@@ -0,0 +1,26 @@
+// monotonic_buffer.cpp
+//
+// Build
+//  cl /EHsc /nologo /std:c++17 /W4 monotonic_buffer.cpp
+
+#include <vector>
+#include <iostream>
+#include <memory_resource>
+
+constexpr static auto const POOL_SIZE = 128;
+
+int main()
+{
+    char buffer[POOL_SIZE] = {};
+    std::fill_n(std::begin(buffer), std::size(buffer)-1, '-');
+    std::cout << buffer << '\n';
+
+    auto pool = std::pmr::monotonic_buffer_resource{std::data(buffer), std::size(buffer)};
+
+    auto v = std::pmr::vector<char>{&pool};
+    for (auto c = 'a'; c <= 'z'; ++c)
+    {
+        v.push_back(c);
+    }
+    std::cout << buffer << '\n';
+}
