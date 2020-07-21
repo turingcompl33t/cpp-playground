cb20954d9d15823d36ccfb2d65dbb9eb0b5ee9c2
turingcompl33t <turingcompl33t@gmail.com>
2020-07-20 21:26:33 -0400

    got expected to compile

--- Utilities/Expected/expected.cpp
+++ Utilities/Expected/expected.cpp
@@ -1,9 +1,30 @@
 // expected.cpp
 //
 // Build
-//  gcc -Wall -Werror -std=c++2a -I${CPP_WORKSPACE}/_Deps/expected expected.cpp -o expected.out
+//  gcc -Wall -Werror -std=c++2a -I${CPP_WORKSPACE}/_Deps/expected/include expected.cpp -o expected.out
+
+#include <cassert>
+#include <system_error>
+
+#include <tl/expected.hpp>
+
+static tl::expected<int, std::errc> test_fn(bool success)
+{
+    if (success)
+    {
+        return 1337;
+    }
+    else
+    {
+        return tl::unexpected(std::errc::bad_file_descriptor);
+    }
+}
 
 int main()
 {
-    return 0;
+    auto const e1 = test_fn(true);
+    auto const e2 = test_fn(false);
+
+    assert(e1.has_value());
+    assert(!e2.has_value());
 }
