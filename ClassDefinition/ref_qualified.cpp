6e3c8c0a03f3e7ba12fbc45765ee96108d7f9b96
turingcompl33t <turingcompl33t@gmail.com>
2020-08-21 09:22:28 -0400

    class definition

--- ClassDefinition/ref_qualified.cpp
+++ ClassDefinition/ref_qualified.cpp
@@ -1,30 +1,63 @@
 // ref_qualified.cpp
 
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
 #include <tuple>
+#include <utility>
 #include <cstdlib>
 #include <iostream>
 
-class SomeObject
+enum class MemberType
 {
-    std::size_t lvalue_count;
-    std::size_t rvalue_count;
-
-public:
-    SomeObject() 
-        : lvalue_count{0}
-        , rvalue_count{0}
-    {}
-
-    void SomeMethod() & { lvalue_count++; }
-    void SomeMethod() && { rvalue_count++; }
-
-    std::pair<std::size_t, std::size_t> GetCounts() const noexcept
-    {
-        return std::make_pair(lvalue_count, rvalue_count);
-    }
+    LValueQualified,
+    RValueQUalified
 };
 
-int main()
+class SomeObject
 {
-    return EXIT_SUCCESS;
+public:
+    SomeObject()  = default;
+    ~SomeObject() = default;
+
+    SomeObject(SomeObject const&)            = delete;
+    SomeObject& operator=(SomeObject const&) = delete;
+
+    SomeObject(SomeObject&&)            = default;
+    SomeObject& operator=(SomeObject&&) = default;
+
+    MemberType SomeMethod() & { return MemberType::LValueQualified; }
+    MemberType SomeMethod() && { return MemberType::RValueQUalified; }
+};
+
+SomeObject make_some_object()
+{
+    return SomeObject{};
+}
+
+TEST_CASE("the lvalue ref-qualified method is invoked")
+{
+    SomeObject o{};
+    auto const member_type = o.SomeMethod();
+
+    REQUIRE(member_type == MemberType::LValueQualified);
+}
+
+TEST_CASE("the rvalue ref-qualified method is invoked")
+{
+    SECTION("with return from factory function")
+    {
+        SomeObject o{};
+        auto const member_type = make_some_object().SomeMethod();
+
+        REQUIRE(member_type == MemberType::RValueQUalified);
+    }
+
+    SECTION("with explicit std::move")
+    {
+        SomeObject o{};
+        auto const member_type = std::move(o).SomeMethod();
+
+        REQUIRE(member_type == MemberType::RValueQUalified);
+    }
 }
